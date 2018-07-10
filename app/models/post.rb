6be303034df25b299be125f31a3a4f981a07a05e
Zac Willmington <zac.willmington@gmail.com>
2018-07-10 01:00:37 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,11 @@
 class Post < ActiveRecord::Base
 
   belongs_to :author
-  validate :is_title_case 
+  validate :is_title_case
 
+  before_validation :make_title_case
+
+  # before_save :email_author_about_post
   private
 
   def is_title_case
